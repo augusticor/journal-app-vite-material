1d50f2d45849f7a1bb7ac882ee85106033bef36c
augusticor <rojascruzoscar@gmail.com>
2022-10-06 18:28:26 -0500

    Finished testing journal thunks

--- tests/store/journal/thunks.test.js
+++ tests/store/journal/thunks.test.js
@@ -1,11 +1,22 @@
-import { deleteAllNotesFromAnUser } from '../../../src/firebase/provides';
-import { addNewEmptyNote, savingNewNote, setActiveNote, setNotes } from '../../../src/store/journal/journalSlice';
-import { startCreatingNote, startLoadingNotes } from '../../../src/store/journal/thunks';
-import { emptyNote } from '../../fixtures/journalFixtures';
+import { deleteAllNotesFromAnUser, setNewNote } from '../../../src/firebase/provides';
+import {
+  addNewEmptyNote,
+  deleteNoteByID,
+  savingNewNote,
+  setActiveNote,
+  setNotes,
+  setSaving,
+  updateNote,
+} from '../../../src/store/journal/journalSlice';
+import { startCreatingNote, startDeletingNote, startLoadingNotes, startSavingNote } from '../../../src/store/journal/thunks';
+import { activeNote, emptyNote } from '../../fixtures/journalFixtures';
 
 describe('Tests on Journal thunks', () => {
   const dispatch = jest.fn();
   const getState = jest.fn();
+
+  // Real user UID from Firebase
+  const testUID = '3d6558rf8AS46Fd5D46';
 
   beforeEach(() => jest.clearAllMocks());
 
@@ -30,14 +41,36 @@ describe('Tests on Journal thunks', () => {
   });
 
   test('Should start loading notes', async () => {
-    // Real user UID from Firebase
-    const testUID = '3d6558rf8AS46Fd5D46';
-
     getState.mockReturnValue({ auth: { uid: testUID } });
 
     await startLoadingNotes()(dispatch, getState);
 
     expect(dispatch).toHaveBeenCalledWith(setNotes(expect.any(Object)));
     expect(dispatch).toHaveBeenCalledTimes(1);
+  });
+
+  test('Should start saving note', async () => {
+    getState.mockReturnValue({ auth: { uid: testUID }, journal: { activeNote: activeNote } });
+
+    await startSavingNote()(dispatch, getState);
+
+    expect(dispatch).toHaveBeenCalledWith(setSaving());
+    expect(dispatch).toHaveBeenCalledWith(updateNote(activeNote));
+    expect(dispatch).toHaveBeenCalledTimes(2);
+  });
+
+  test('Should start deleting note', async () => {
+    const newNote = { title: '', body: '', date: new Date().getTime(), imagesUrls: [] };
+
+    const newNoteReferenceID = await setNewNote(newNote, `${testUID}/journal/notes`);
+    newNote.id = newNoteReferenceID;
+
+    getState.mockReturnValue({ auth: { uid: testUID }, journal: { activeNote: newNote } });
+
+    await startDeletingNote()(dispatch, getState);
+
+    expect(dispatch).toHaveBeenCalledWith(setSaving());
+    expect(dispatch).toHaveBeenCalledWith(deleteNoteByID(expect.any(Object)));
+    expect(dispatch).toHaveBeenCalledTimes(2);
   });
 });
